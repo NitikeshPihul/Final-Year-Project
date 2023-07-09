0ec8c0ef8e1a623425204bb042d8bb10d0db3364
NitikeshPihul <74096657+NitikeshPihul@users.noreply.github.com>
2023-07-09 15:04:48 +0530

    Update Copy.java

--- Copy.java
+++ Copy.java
@@ -22,3 +22,67 @@ private static File[] getFilesFromResources(String folderPath) throws IOExceptio
         return fileList.toArray(new File[0]);
     }
 }
+
+
+
+import java.io.*;
+import java.nio.file.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Specify the path to the folder relative to the resources directory
+        String folderPath = "templates";
+
+        // Specify the destination path where the folder will be copied
+        String destinationPath = "path/to/destination/folder";
+
+        copyFolderFromResources(folderPath, destinationPath);
+
+        System.out.println("Folder copied successfully.");
+    }
+
+    private static void copyFolderFromResources(String folderPath, String destinationPath) {
+        try {
+            // Create the destination folder if it doesn't exist
+            File destinationFolder = new File(destinationPath);
+            if (!destinationFolder.exists()) {
+                destinationFolder.mkdirs();
+            }
+
+            // Get the list of files in the folder from the resources
+            File[] files = getFilesFromResources(folderPath);
+
+            // Copy each file to the destination folder
+            for (File file : files) {
+                String relativePath = folderPath + "/" + file.getName();
+                String destinationFilePath = destinationPath + "/" + file.getName();
+
+                try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(relativePath);
+                     OutputStream outputStream = new FileOutputStream(destinationFilePath)) {
+
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = inputStream.read(buffer)) > 0) {
+                        outputStream.write(buffer, 0, length);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static File[] getFilesFromResources(String folderPath) throws IOException {
+        try (InputStream inputStream = Main.class.getClassLoader().getResourceAsStream(folderPath);
+             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+
+            if (inputStream == null) {
+                throw new IOException("Folder not found: " + folderPath);
+            }
+
+            return reader.lines()
+                    .map(line -> new File(line))
+                    .toArray(File[]::new);
+        }
+    }
+}
