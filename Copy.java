a8f9a568eaf4911387f789527dab105d95d316a8
NitikeshPihul <74096657+NitikeshPihul@users.noreply.github.com>
2023-07-11 16:24:24 +0530

    Update Copy.java

--- Copy.java
+++ Copy.java
@@ -201,4 +201,66 @@ public class CopyFilesFromJarExample {
 }
 
 
-    
+ +++++++++++++++++++++++
+
+
+     import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class FolderToZipConverter {
+
+    public static void main(String[] args) {
+        String sourceFolderPath = "path/to/versionTree";  // Path to the source folder
+        String zipFilePath = "path/to/output.zip";        // Path to the output zip file
+
+        zipFolder(sourceFolderPath, zipFilePath);
+    }
+
+    public static void zipFolder(String sourceFolderPath, String zipFilePath) {
+        try {
+            FileOutputStream fos = new FileOutputStream(zipFilePath);
+            ZipOutputStream zos = new ZipOutputStream(fos);
+
+            addFolderToZip(sourceFolderPath, sourceFolderPath, zos);
+
+            zos.close();
+            fos.close();
+
+            System.out.println("Folder successfully compressed into zip file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void addFolderToZip(String folderPath, String sourceFolderPath, ZipOutputStream zos) throws IOException {
+        File folder = new File(folderPath);
+        File[] files = folder.listFiles();
+
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    addFolderToZip(file.getAbsolutePath(), sourceFolderPath, zos);
+                } else {
+                    String relativePath = file.getAbsolutePath().substring(sourceFolderPath.length() + 1);
+                    ZipEntry zipEntry = new ZipEntry(relativePath);
+                    zos.putNextEntry(zipEntry);
+
+                    FileInputStream fis = new FileInputStream(file);
+                    byte[] buffer = new byte[1024];
+                    int length;
+                    while ((length = fis.read(buffer)) > 0) {
+                        zos.write(buffer, 0, length);
+                    }
+
+                    fis.close();
+                    zos.closeEntry();
+                }
+            }
+        }
+    }
+}
+
